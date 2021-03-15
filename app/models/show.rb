9a7ad42dd0b4072e0a7a21731f6df3c95f9c9272
Silas Michael Knight <silasfelinus@gmail.com>
2021-03-15 01:09:06 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,37 @@
 class Show < ActiveRecord::Base
+  attr_accessor :season
+
+  def self.highest_rating
+    Show.maximum(:rating)
+  end
+
+  def self.most_popular_show
+    Show.where("rating = ?", Show.maximum(:rating))[0]
+  end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where("rating = ?", Show.minimum(:rating))[0]
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > 5")
+  end
+
+  def self.shows_by_alphabetical_order
+    Show.order(:name)
+
+  end
+
+
+
 
 
 
